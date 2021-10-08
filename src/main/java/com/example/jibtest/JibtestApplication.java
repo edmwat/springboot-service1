dc79bdf38d7495d9c70eedb60935b7242d1c1b6b
edmwat <edmwat@gmail.com>
2021-10-08 22:36:28 +0300

    added service-service comm

--- src/main/java/com/example/jibtest/JibtestApplication.java
+++ src/main/java/com/example/jibtest/JibtestApplication.java
@@ -2,6 +2,8 @@ package com.example.jibtest;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class JibtestApplication {
@@ -9,5 +11,8 @@ public class JibtestApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(JibtestApplication.class, args);
 	}
-
+	@Bean
+	public RestTemplate restTemplate() {
+	    return new RestTemplate();
+	}
 }
--- src/main/java/com/example/jibtest/Welcome.java
+++ src/main/java/com/example/jibtest/Welcome.java
@@ -1,19 +1,35 @@
 package com.example.jibtest;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 @CrossOrigin(origins = "https://angular8-application-lhefrhbyia-uc.a.run.app", maxAge = 3600)
 @RestController
 @RequestMapping(path="api/home")
 public class Welcome {
-	
+	@Autowired 
+	RestTemplate restTemplate;
+	 
 	@GetMapping
-	public Greeting welcome() {
-		Greeting greet = new Greeting("Hello, message from CEO,Edward MM "," Enjoy my one command dockerization!");
-		return greet;
+	public Object welcome() {
+		try {
+			HttpHeaders headers = new HttpHeaders();
+			HttpEntity <String> entity = new HttpEntity<String>(headers);
+			return restTemplate.exchange(
+					"https://springservice2-lhefrhbyia-uc.a.run.app/api/gfactory", HttpMethod.GET, entity, Greeting.class).getBody();
+			//Greeting greet = new Greeting("Hello, message from CEO,Edward MM "," Enjoy my one command dockerization!");
+			//return greet;		
+		}catch(Exception ex) {
+			return ex;
+			//System.out.println(ex.getMessage());
+		}
 		
 	}
 }
