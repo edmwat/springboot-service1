346e71163a00e3141399e3eb89824dbab5c4b48e
edmwat <edmwat@gmail.com>
2021-10-09 16:38:32 +0300

    updated webclient from restTemplate

--- src/main/java/com/example/jibtest/Welcome.java
+++ src/main/java/com/example/jibtest/Welcome.java
@@ -1,33 +1,35 @@
 package com.example.jibtest;
 
+import java.util.Collections;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.client.RestTemplate;
+import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.publisher.Mono;
 
-@CrossOrigin(origins = {"https://angular8-application-lhefrhbyia-uc.a.run.app","http://localhost:8080/api/home"}, maxAge = 3600)
+@CrossOrigin(origins = {"https://angular8-application-lhefrhbyia-uc.a.run.app","http://localhost:8080"}, maxAge = 3600)
 @RestController
 @RequestMapping(path="api/home")
 public class Welcome {
-	 
-	@GetMapping
-	public Greeting welcome() {
-		
-		Greeting greet = new Greeting("Hello, message from CEO,Edward MM ","Enjoy my one command dockerization!");
-		return greet;	
-		/*
-		 * HttpHeaders headers = new HttpHeaders(); HttpEntity <String> entity = new
-		 * HttpEntity<String>(headers);
-		 * 
-		 * try {
-		 * 
-		 * g = restTemplate.exchange(
-		 * "https://springservice2-lhefrhbyia-uc.a.run.app/api/gfactory",
-		 * HttpMethod.GET, entity, Greeting.class).getBody(); }catch(Exception ex) {
-		 * System.out.println(ex.getMessage()); } return g;
-		 */
-		 
+	
+	private final WebClient client;
+	
+	public Welcome() {
+		client = WebClient.builder()
+		        .baseUrl("https://springservice2-lhefrhbyia-uc.a.run.app")
+		        .defaultHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
+		        .build();
+	}
+	@GetMapping 
+	public Mono<Greeting> welcome() {
+		return client.get()
+		        .uri("/api/gfactory")
+		        .retrieve()
+		        .bodyToMono(Greeting.class);		 
 	}
 }
